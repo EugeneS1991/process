775c47467cc212f026725ef62aee83e6933ee46d
Yevhen_Skotarenko <test@test.ua>
2022-09-04 11:21:03 +0300

    add logging date for testing requests

--- app.py
+++ app.py
@@ -15,8 +15,9 @@ def log():
     app.logger.info(raw_req_data.get_json())
     app.logger.info(type(raw_req_data))
     app.logger.info(type(payload))
+    app.logger.info('testinger')
     return 'Received task with payload: {}'.format(payload)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, threaded=True, debug=False)
+    app.run(host='0.0.0.0', port=8080, threaded=True, debug=True)
